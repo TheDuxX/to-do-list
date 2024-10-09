08019dfb48119e2dc9e694c1831bc9121725d9dc
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2024-10-09 17:22:10 -0300

    feat: fix imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,3 @@
-import Hero from "@/app/_components/hero";
-import ConnectSupabaseSteps from "@/app/_components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/app/_components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
 export default async function Index() {
@@ -8,7 +5,7 @@ export default async function Index() {
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+        {/* {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />} */}
       </main>
     </>
   );
