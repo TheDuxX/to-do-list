7ba096a06520ef4f709d2ba24db2d3d8f5baaa09
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2024-10-10 15:37:47 -0300

    feat: add shadcn ui

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,19 @@
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+import HeaderNav from "../_components/header";
+import { ThemeSwitcher } from "../_components/theme-switcher";
 
 export default async function Index() {
+  const supabase = createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    redirect("/login");
+  }
+
   return (
     <>
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {/* {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />} */}
-      </main>
+      <HeaderNav />
     </>
   );
 }
