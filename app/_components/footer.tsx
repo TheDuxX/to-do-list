93c9afb856739ce8392e30baa7f054d9fd72fae2
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2024-10-17 16:03:37 -0300

    feat: init footer component

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,7 +1,25 @@
+import { Calendar, CheckSquare, HomeIcon, User2Icon } from "lucide-react";
+
 const FooterMenu = () => {
   return (
     <>
-      <div className="bg-primary w-full fixed bottom-0"> teste</div>
+      <div className="bg-primary w-full fixed bottom-0 flex flex-row justify-around items-center text-white py-3 font-extralight text-sm">
+        <div className="flex flex-col items-center justify-center ">
+          <HomeIcon size={20}/>
+          <p>Início</p>
+        </div>
+        <div className="flex flex-col items-center justify-center ">
+          <Calendar size={20}/>
+          <p>Agenda</p>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <CheckSquare size={20}/>
+          <p>Listas</p>
+        </div><div className="flex flex-col items-center justify-center">
+          <User2Icon size={20}/>
+          <p>Perfil</p>
+        </div>
+      </div>
     </>
   );
 };
