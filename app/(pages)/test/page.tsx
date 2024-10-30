71b5cff655e8ff203bf1a6467d61ba629b67c92d
TheDuxX <125306500+TheDuxX@users.noreply.github.com>
2024-10-30 17:32:58 -0300

    feat: add test page

---
+++ app/(pages)/test/page.tsx
@@ -0,0 +1,12 @@
+import LastestLists from "@/app/(home)/_component/lists";
+
+const Test = () => {
+  return (
+    <div className="flex flex-col gap-2">
+      <p className="text-center">Teste de Códigos</p>
+      <LastestLists />
+    </div>
+  );
+};
+
+export default Test;
